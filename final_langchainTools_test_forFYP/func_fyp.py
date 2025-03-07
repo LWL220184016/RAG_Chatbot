5cfaa0fd9170dc708b4661e095119c1e7ce280fb
LWL220184016 <145105128+LWL220184016@users.noreply.github.com>
2025-03-07 07:02:01 +0000

    35.4 add test asr only

--- final_langchainTools_test_forFYP/func_fyp.py
+++ final_langchainTools_test_forFYP/func_fyp.py
@@ -4,147 +4,146 @@ import threading
 import torch
 import traceback
 
-# from ASR.audio_process import Audio_Processer
-# # from ASR.asr import ASR
-# from ASR.model_classes.NeMo import ASR
-# from TTS.tts_transformers import TTS
+from TTS.tts_transformers import TTS
 
 
-# def asr_process_func(
-#         stop_event: threading.Event, 
-#         is_user_talking: threading.Event,
-#         is_asr_ready_event: threading.Event,
-#         asr_output_queue: queue, 
-#         ap: Audio_Processer = None, 
-#     ):
-
-#     SOUND_LEVEL = 10
-#     CHUNK = 512
-#     FORMAT = pyaudio.paInt16
-#     CHANNELS = 1
-#     RATE = 16000
-#     TIMEOUT_SEC = 0.3
-
-#     try:
-#         if ap is None:
-#             ap = Audio_Processer(
-#                 chunk=CHUNK, 
-#                 format=FORMAT, 
-#                 channels=CHANNELS, 
-#                 rate=RATE, 
-#                 is_user_talking=is_user_talking, 
-#                 stop_event=stop_event
-#             )
-#         get_audio_thread = threading.Thread(target=ap.get_chunk, args=(True,))
-#         check_audio_thread = threading.Thread(target=ap.detect_sound, args=(SOUND_LEVEL, TIMEOUT_SEC))
-#         get_audio_thread.start()
-#         check_audio_thread.start()
-#         asr = ASR(stop_event=stop_event, ap=ap, asr_output_queue=asr_output_queue)
-#         print("asr_process_func asring")
-#         asr.asr_output(is_asr_ready_event)
-#         print("asr_process_func end")
-
-#     except KeyboardInterrupt:
-#         print("asr_process_func KeyboardInterrupt\n")
-#         get_audio_thread.join()
-#         check_audio_thread.join()
-#         ap.stream.stop_stream()
-#         ap.stream.close()
-#         ap.p.terminate()
-#         torch.cuda.ipc_collect()
-
-#     finally:
-#         print("asr_process_func finally\n")
-#         get_audio_thread.join()
-#         check_audio_thread.join()
-#         torch.cuda.ipc_collect()
-#         ap.stream.stop_stream()
-#         ap.stream.close()
-#         ap.p.terminate()
-
-# def asr_process_func_ws(
-#         stop_event: threading.Event, 
-#         is_user_talking: threading.Event,
-#         is_asr_ready_event: threading.Event,
-#         uncheck_audio_queue: queue, 
-#         asr_output_queue: queue, 
-#         asr_output_queue_ws: queue, 
-#         ap: Audio_Processer = None, 
-#     ):
-    
-#     CHUNK = 512
-#     FORMAT = pyaudio.paInt16
-#     CHANNELS = 1
-#     RATE = 16000
-
-#     try:
-#         if ap is None:
-#             ap = Audio_Processer(
-#                 chunk=CHUNK, 
-#                 format=FORMAT, 
-#                 channels=CHANNELS, 
-#                 rate=RATE, 
-#                 audio_checked_queue=uncheck_audio_queue,
-#                 startStream=False,
-#                 is_user_talking=is_user_talking, 
-#                 stop_event=stop_event,
-#             )
-#         asr = ASR(stop_event=stop_event, ap=ap, asr_output_queue=asr_output_queue)
-#         asr.asr_output_ws(is_asr_ready_event, asr_output_queue_ws)
-#         print("asr_process_func end")
-
-#     except KeyboardInterrupt:
-#         print("asr_process_func KeyboardInterrupt\n")
-#         ap.p.terminate()
-#         torch.cuda.ipc_collect()
-    
-#     except Exception as e:
-#         print("捕获异常：", e)
-#         print("完整的错误信息：")
-#         traceback.print_exc()
-
-#     finally:
-#         print("asr_process_func finally\n")
-#         torch.cuda.ipc_collect()
-#         ap.p.terminate()
-
-def llm_model_process_func_ws(
+def asr_process_func(
+        is_user_talking: threading.Event,
         stop_event: threading.Event, 
-        is_user_talking: threading.Event, 
-        speaking_event: threading.Event, 
-        is_llm_ready_event: threading.Event,
+        is_asr_ready_event: threading.Event,
         asr_output_queue: queue, 
-        llm_output_queue: queue, 
-        llm_output_queue_ws: queue, 
-        prompt_template, 
-        llm = None, 
+        ap = None, 
     ):
-    
+    """
+    ap: Audio_Processer
+    """
+
+    from ASR.audio_process import Audio_Processer
+    # from ASR.model_classes.faster_whisper import ASR
+    from ASR.model_classes.NeMo import ASR
+
+    SOUND_LEVEL = 10
+    CHUNK = 512
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+    TIMEOUT_SEC = 0.3
+
     try:
-        llm.llm_output_ws(
-            is_llm_ready_event=is_llm_ready_event, 
-            prompt_template=prompt_template
-        )
+        if ap is None:
+            ap = Audio_Processer(
+                chunk=CHUNK, 
+                format=FORMAT, 
+                channels=CHANNELS, 
+                rate=RATE, 
+                is_user_talking=is_user_talking, 
+                stop_event=stop_event
+            )
+        get_audio_thread = threading.Thread(target=ap.get_chunk, args=(True,))
+        check_audio_thread = threading.Thread(target=ap.detect_sound, args=(SOUND_LEVEL, TIMEOUT_SEC))
+        get_audio_thread.start()
+        check_audio_thread.start()
+        asr = ASR(stop_event=stop_event, ap=ap, asr_output_queue=asr_output_queue)
+        print("asr_process_func asring")
+        asr.asr_output(is_asr_ready_event)
+        print("asr_process_func end")
+
     except KeyboardInterrupt:
-        print("llm_process_func KeyboardInterrupt\n")
-        stop_event.set()
-    finally:
-        print("llm_process_func finally\n")
-        stop_event.set()
+        print("asr_process_func KeyboardInterrupt\n")
+        get_audio_thread.join()
+        check_audio_thread.join()
+        ap.stream.stop_stream()
+        ap.stream.close()
+        ap.p.terminate()
         torch.cuda.ipc_collect()
 
-def llm_agent_process_func_ws(
+    finally:
+        print("asr_process_func finally\n")
+        get_audio_thread.join()
+        check_audio_thread.join()
+        torch.cuda.ipc_collect()
+        ap.stream.stop_stream()
+        ap.stream.close()
+        ap.p.terminate()
+
+def asr_process_func_ws(
+        is_user_talking: threading.Event,
+        stop_event: threading.Event, 
+        is_asr_ready_event: threading.Event,
+        uncheck_audio_queue: queue, 
+        asr_output_queue: queue, 
+        asr_output_queue_ws: queue, 
+        ap = None, 
+    ):
+    """
+    ap: Audio_Processer
+    """
+
+    from ASR.audio_process import Audio_Processer
+    # from ASR.model_classes.faster_whisper import ASR
+    from ASR.model_classes.NeMo import ASR
+    
+    CHUNK = 512
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+
+    try:
+        if ap is None:
+            ap = Audio_Processer(
+                chunk=CHUNK, 
+                format=FORMAT, 
+                channels=CHANNELS, 
+                rate=RATE, 
+                audio_checked_queue=uncheck_audio_queue,
+                startStream=False,
+                is_user_talking=is_user_talking, 
+                stop_event=stop_event,
+            )
+        asr = ASR(stop_event=stop_event, ap=ap, asr_output_queue=asr_output_queue)
+        asr.asr_output_ws(is_asr_ready_event, asr_output_queue_ws)
+        print("asr_process_func end")
+
+    except KeyboardInterrupt:
+        print("asr_process_func KeyboardInterrupt\n")
+        ap.p.terminate()
+        torch.cuda.ipc_collect()
+    
+    except Exception as e:
+        print("捕获异常：", e)
+        print("完整的错误信息：")
+        traceback.print_exc()
+
+    finally:
+        print("asr_process_func finally\n")
+        torch.cuda.ipc_collect()
+        ap.p.terminate()
+
+def llm_process_func_ws( 
         stop_event: threading.Event, 
         is_user_talking: threading.Event, 
         speaking_event: threading.Event, 
-        is_llm_ready_event: threading.Event,
+        is_llm_ready_event: threading.Event, 
         asr_output_queue: queue, 
         llm_output_queue: queue, 
         llm_output_queue_ws: queue, 
         prompt_template, 
         llm = None, 
+        use_agent = False, 
     ):
+
+    # from LLM.llm_transformers import LLM_Transformers as LLM
     from LLM.llm_google import LLM_Google as LLM
+    # from LLM.llm_ollama import LLM_Ollama as LLM
+    from Data_Storage.qdrant import Qdrant_Handler as Database
+    from Tools.tool import Tools
+
+    tools = [
+        Tools.duckduckgo_search, 
+        Tools.querying_qdrant, 
+        Tools.get_current_dateTime
+    ]
+    database = Database()
 
     llm = LLM( 
         # model_name="deepseek-r1_14b_FYP4", 
@@ -156,67 +155,20 @@ def llm_agent_process_func_ws(
         user_input_queue=asr_output_queue, 
         llm_output_queue=llm_output_queue, 
         llm_output_queue_ws=llm_output_queue_ws, 
-        # tools=tools, 
-        # database=database, 
+        tools=tools, 
+        database=database, 
     ) 
-    
     try:
-        llm.agent_output_ws(
-            is_llm_ready_event=is_llm_ready_event, 
-            prompt_template=prompt_template
-        )
-    except KeyboardInterrupt:
-        print("llm_process_func KeyboardInterrupt\n")
-        stop_event.set()
-    finally:
-        print("llm_process_func finally\n")
-        stop_event.set()
-        torch.cuda.ipc_collect()
-
-# 從資料庫取得聊天記錄
-def llm_memory_model_process_func_ws(
-        stop_event: threading.Event, 
-        is_user_talking: threading.Event, 
-        speaking_event: threading.Event, 
-        is_llm_ready_event: threading.Event,
-        asr_output_queue: queue, 
-        llm_output_queue: queue, 
-        llm_output_queue_ws: queue, 
-        prompt_template, 
-        llm = None, 
-    ):
-    
-    try:
-        llm.llm_memory_output_ws(
-            is_llm_ready_event=is_llm_ready_event, 
-            prompt_template=prompt_template
-        )
-    except KeyboardInterrupt:
-        print("llm_process_func KeyboardInterrupt\n")
-        stop_event.set()
-    finally:
-        print("llm_process_func finally\n")
-        stop_event.set()
-        torch.cuda.ipc_collect()
-
-# 從資料庫取得聊天記錄
-def llm_memory_agent_process_func_ws(
-        stop_event: threading.Event, 
-        is_user_talking: threading.Event, 
-        speaking_event: threading.Event, 
-        is_llm_ready_event: threading.Event,
-        asr_output_queue: queue, 
-        llm_output_queue: queue, 
-        llm_output_queue_ws: queue, 
-        prompt_template, 
-        llm = None, 
-    ):
-    
-    try:
-        llm.agent_memory_output_ws(
-            is_llm_ready_event=is_llm_ready_event, 
-            prompt_template=prompt_template
-        )
+        if use_agent:
+            llm.agent_output_ws(
+                is_llm_ready_event=is_llm_ready_event, 
+                prompt_template=prompt_template
+            )
+        else:
+            llm.llm_output_ws(
+                is_llm_ready_event=is_llm_ready_event, 
+                prompt_template=prompt_template
+            )
     except KeyboardInterrupt:
         print("llm_process_func KeyboardInterrupt\n")
         stop_event.set()
